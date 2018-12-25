f8d2ee29e58411f386137e730f0a51d6928bf307
Nora <fanora88@gmail.com>
2018-12-25 12:11:57 +0100

    commonJS extending

--- common.js
+++ common.js
@@ -34,3 +34,69 @@ function CreateElement(elemOptions, parent) {
   parent.appendChild(htmlElement);
   return htmlElement;
 }
+
+/**
+ * 
+ * @param {Object} obj 
+ * @param {HTMLElement} parent 
+ */
+function createInfoList(obj, parent) {
+  var item = CreateElement({
+    tagName: "li",
+    innerText: obj.title
+  }, parent);
+  if (obj.type) {
+    CreateElement({
+      tagName: "span",
+      className: "type",
+      innerText: obj.type
+    }, item);
+  }
+  if (obj.description) {
+    CreateElement({
+      tagName: "span",
+      className: "description",
+      innerText: obj.description
+    }, item);
+
+  }
+  if (obj.list) {
+    var list = document.createElement("ul");
+    obj.list.forEach(function (element) {
+      var subItem = createInfoList(element, list);
+      item.appendChild(list);
+    });
+  }
+}
+
+/**
+ * Print description tree
+ * @param {Object} obj (title, type, description)
+ */
+function printInfo(obj) {
+  var paragraph = document.createElement("p");
+  CreateElement({
+    innerText: obj.title,
+    tagName: "b"
+  }, paragraph);
+  obj.list.forEach(function (element) {
+    createInfoList(element, paragraph);
+  });
+  return paragraph;
+}
+
+/**
+ * 
+ * @param {*} value Value to push
+ * @param {*} type Type of array element 
+ * @param {Number} length  Array length
+ */
+function ArrayGenerator(value, type, length) {
+  let arr = [];
+  for (var l = 0; l < length; l++) {
+    arr.push(type.toLowerCase() === "string" ? value + " " + (l + 1) : {
+      title: value + " " + (l + 1)
+    });
+  }
+  return arr;
+}
