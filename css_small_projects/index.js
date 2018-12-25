44c87343bbca4beff3ee7d0fe8bd410211186b91
Nora <fanora88@gmail.com>
2018-12-25 12:13:33 +0100

    description generator an other modifications

--- css_small_projects/index.js
+++ css_small_projects/index.js
@@ -1,46 +1,114 @@
-var radialList = [{
-		title: "SubElement 1"
-	},
-	{
-		title: "SubElement 2"
-	},
-	{
-		title: "SubElement 3"
-	},
-	{
-		title: "SubElement 4"
-	},
-	{
-		title: "SubElement 5"
-	},
-	{
-		title: "SubElement 6"
-	},
-	{
-		title: "SubElement 7"
-	},
-	{
-		title: "SubElement 8"
-	}
-];
-
-function ArrayGenerator(text, length) {
-	let arr = [];
-	for (var l = 0; l < length; l++) {
-		arr.push({
-			title: text + " " + l
-		});
-	}
-	return arr;
+// Radial menu description
+radialMenuInfo = {
+	title: "Radial menu",
+	list: [{
+			title: "Initialize:",
+			list: [{
+				title: "Parameter options:",
+				list: [{
+						title: "list",
+						type: "Array",
+						description: "array of menu element",
+						list: [{
+								title: "If each element is an",
+								type: "Object",
+								list: [{
+										title: "object.title",
+										type: "String",
+										description: "Menu's text in <span> child"
+									},
+									{
+										title: "Additional attributes: .."
+									}
+								]
+							},
+							{
+								title: "If each element is a",
+								type: "String",
+								description: "No options availible"
+							}
+						]
+					},
+					{
+						title: "menu",
+						type: "String",
+						description: "ID of menu (default: radial-menu)"
+					},
+					{
+						title: "mainText",
+						type: "String",
+						description: "Main menu's text (default: Main menu)"
+					},
+					{
+						title: "main",
+						type: "String",
+						description: "Main menu class (default: main-menu)"
+					},
+					{
+						title: "subList",
+						type: "String",
+						description: "Submenus' container class (default: sub-menu-container)"
+					},
+					{
+						title: "subItemClassName",
+						type: "String",
+						description: "Submenus' class (default: sub-menu-item)"
+					},
+					{
+						title: "subMenuWidth",
+						type: "Number",
+						description: "Submenus' width (default: 80)"
+					},
+					{
+						title: "subMenuWidthUnit",
+						type: "String",
+						description: "Unit type of submenus' widthe (default: px)"
+					},
+					{
+						title: "subMenuContainerPadding",
+						type: "Number",
+						description: "Padding of submenus' container (default: 50)"
+					},
+					{
+						title: "subMenuContainerPaddingUnit",
+						type: "String",
+						description: "Unit tpye of submenus' container padding (default: px)"
+					},
+					{
+						title: "levels",
+						type: "Number",
+						description: "Number of levels (default: 1 === none)"
+					},
+					{
+						title: "currentLevel",
+						type: "Number",
+						description: "Can be set as the starter level of first submenu (default: 1)"
+					}
+				]
+			}]
+		},
+		{
+			title: "Default CSS selectors",
+			list: [{
+				title: ""
+			}]
+		}
+	]
 }
 
+Array.from(document.getElementsByClassName("description")).forEach(function (element) {
+	if (element.classList.contains("radialMenuInfo")) {
+		var info = printInfo(radialMenuInfo);
+		element.appendChild(info);
+	}
+});
+
 document.querySelector(".radial-menu-container").radialMenu({
-	list: radialList,
-	mainText: "My main"
+	list: ArrayGenerator("Subelement", "string", 8),
 });
 
 document.querySelector(".radial-menu-container-2").radialMenu({
-	list: ArrayGenerator("Custom text", 20),
+	list: ArrayGenerator("Custom text", "object", 20),
 	mainText: "Main",
 	levels: 2
 });
