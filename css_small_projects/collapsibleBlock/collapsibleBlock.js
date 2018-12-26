1f352a5268518e25edef4eb946f489391c7c0368
Nora <fanora88@gmail.com>
2018-12-26 13:00:22 +0100

    collapsible block startup

---
+++ css_small_projects/collapsibleBlock/collapsibleBlock.js
@@ -0,0 +1,10 @@
+HTMLElement.prototype.collapsibleBlock = function (options) {
+  var collapsibleBlock = {
+    title: "Collpasible"
+  }
+
+  // get innerHTML to place in the body
+  var body = this.innerHTML;
+  // add title block as header
+
+}
