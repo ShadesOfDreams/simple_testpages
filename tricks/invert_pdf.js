e3c600d8a414588b4559bb941e76f60b651965a3
Nora <fabiankovits.nora@effsystem.eu>
2021-08-23 11:14:15 +0200

    added tricks folder

---
+++ tricks/invert_pdf.js
@@ -0,0 +1,31 @@
+/**
+ * Open pdf in chrome and paste into console to invert
+ */
+
+var cover = document.createElement("div");
+
+let css = `
+
+position: fixed;
+
+pointer-events: none;
+
+top: 0;
+
+left: 0;
+
+width: 100vw;
+
+height: 100vh;
+
+background-color: white;
+
+mix-blend-mode: difference;
+
+z-index: 1;
+
+`
+
+cover.setAttribute("style", css);
+
+document.body.appendChild(cover);
